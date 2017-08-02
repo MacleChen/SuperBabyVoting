21597b7e8c01eceb6380f938cc9dcdc497b4e8df
JackStraw <851327579@qq.com>
2017-08-02 17:41:58 +0800

    version 0.0.0.1 更新框架结构

--- codes/app.js
+++ codes/app.js
@@ -1,34 +1,36 @@
 //app.js
-
-var indexData = {
-  myTextName: "chenfan"
-}
-
 App({
-  onLaunch: function() {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-
-  getUserInfo: function(cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function(res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
+    onLaunch: function () {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs);
+        wx.getSystemInfo({
+            success: function (res) {
+                console.log("屏幕高度" + res.windowHeight);
+                wx.setStorageSync("windowHeight", res.windowHeight)
+            }
+        })
+    },
+    getUserInfo: function (cb) {
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function () {
+                    wx.getUserInfo({
+                        success: function (res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
         }
-      })
+    },
+    globalData: {
+        userInfo: null
     }
-  },
-
-  globalData: {
-    userInfo: null
-  }
 })
--- codes/pages/index/index.js
+++ codes/pages/index/index.js
@@ -1,41 +1,68 @@
 //index.js
 //获取应用实例
 var app = getApp()
-
-// register a page
-Page({
+Page( {
   data: {
-    motto: 'hello world',
-    userInfo: {},
-    myTextName: 'chenfan'
+    windowHeight: 654,
+    maxtime: "",
+    isHiddenLoading: false,
+    isHiddenToast: true,
+    dataList: {}
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
+    wx.navigateTo( {
       url: '../logs/logs'
     })
   },
+  onLoad: function() {
+    this.setData( {
+      windowHeight: wx.getStorageSync( 'windowHeight' )
+    });
 
-  // 更改姓名处理函数
-  changMyName: function(e) {
-    wx.showToast({
-      title: '点我干嘛',
-    })
-    this.setData({
-      myTextName: 'OK!'
-    })
+    this.requestData( "newlist" );
+
   },
-
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      // 更改头像数据
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  requestData: function( a ) {
+    var that = this;
+    //  请求数据
+    wx.request( {
+      url: "http://api.budejie.com/api/api_open.php",
+      data: {
+        a: a,
+        c: "data",
+        maxtime: that.data.maxtime,
+        type: 29
+      },
+      method: "GET",
+      success: function( res ) {
+        console.log( res );
+        that.setData( {
+          isHiddenLoading: true,
+          dataList: res.data.list,
+          maxtime: res.data.info.maxtime,
+          isHiddenToast: false
+        });
+        console.log( "成功了" );
+      },
+      fail: function() {
+        console.log( "失败了" );
+      }
+    });
+  },
+  // 下拉刷新
+  upper: function( e ) {
+    console.log( "下拉刷新了" );
+    this.requestData( "newlist" );
+  },
+  // 加载 
+  lower: function( e ) {
+    console.log( "加载更多了" );
+    this.requestData( "list" );
+  },
+  closeToast: function( e ) {
+    this.setData( {
+      isHiddenToast: true
+    });
   }
 })
