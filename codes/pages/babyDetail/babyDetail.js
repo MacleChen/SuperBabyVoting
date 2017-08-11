f1e5fa7968774cf390f36f967a2d7033a6adac0a
JackStraw <851327579@qq.com>
2017-08-11 17:27:40 +0800

    version 0.0.0.10 1. 增加心跳动画 2. 增加倒计时

--- codes/pages/babyDetail/babyDetail.js
+++ codes/pages/babyDetail/babyDetail.js
@@ -9,9 +9,10 @@ Page({
         currentSwiperIndex: 0,
         isHiddenLoading: true,
         isHiddenToast: true,
+        animationMiddleHeaderItem: '',
         dataList: {}
     },
-    //获取列表残过来的参数 id
+    //获取列表残过来的参数 id   (页面初始化方法)
     onLoad: function (options) {
       var id = options.id;
       wx.showToast({
@@ -23,6 +24,51 @@ Page({
         windowHeight: wx.getStorageSync('windowHeight')
       });
     },
+
+    // 页面渲染完成
+    onReady: function () {
+      var circleCount = 0;
+      // 心跳的外框动画
+      this.animationMiddleHeaderItem = wx.createAnimation({
+        duration:1000,    // 以毫秒为单位
+        /**
+       * http://cubic-bezier.com/#0,0,.58,1  
+       *  linear  动画一直较为均匀
+       *  ease    从匀速到加速在到匀速
+       *  ease-in 缓慢到匀速
+       *  ease-in-out 从缓慢到匀速再到缓慢
+       * 
+       *  http://www.tuicool.com/articles/neqMVr
+       *  step-start 动画一开始就跳到 100% 直到动画持续时间结束 一闪而过
+       *  step-end   保持 0% 的样式直到动画持续时间结束        一闪而过
+       */
+        timingFunction: 'linear',
+        delay: 100,
+        transformOrigin: '50% 50%',
+        success: function (res) {
+        }
+      });
+      
+      var interval = setInterval(function() {
+        if (circleCount % 2 == 0) {
+          this.animationMiddleHeaderItem.scale(1.15).step();
+        } else {
+          this.animationMiddleHeaderItem.scale(1.0).step();
+        }
+        
+        this.setData({
+          animationMiddleHeaderItem: this.animationMiddleHeaderItem.export()
+        });
+        
+        circleCount++;
+        if (circleCount == 1000) {
+          circleCount = 0;
+        }
+      }.bind(this), 1000);
+      
+    },
+
+
 
     // swiper change
     swiperBindChange: function (e) {
@@ -59,5 +105,10 @@ Page({
           currentSwiperIndex: current + 1,
         });
       }
-    }
+    },
+
+    // 投票按钮点击
+    voteClick: function (e) {
+      
+    },
 })
