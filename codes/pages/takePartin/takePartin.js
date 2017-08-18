e03091516c6788eb46599a48017e8be8e4db3f3f
JackStraw <851327579@qq.com>
2017-08-18 17:35:05 +0800

    version 0.0.0.14 1. 增加本地相册图片选择

--- codes/pages/takePartin/takePartin.js
+++ codes/pages/takePartin/takePartin.js
@@ -7,12 +7,8 @@ Page( {
     maxtime: "",
     isHiddenLoading: true,
     isHiddenToast: true,
-    dataList: {},
-    countDownDay: 0,
-    countDownHour: 0,
-    countDownMinute: 0,
-    countDownSecond: 0,
-    searchInputText: '',
+    selectedImageFilePaths: [],
+    coverIndex: 0,  // 封面索引
   },
   //事件处理函数
   bindViewTap: function() {
@@ -22,8 +18,35 @@ Page( {
   },
   onLoad: function() {
     this.setData( {
-      windowHeight: wx.getStorageSync( 'windowHeight' ),
+      windowHeight: wx.getStorageSync( 'windowHeight' ) + 44,
     });
+  },
+
+
+  // 添加图片
+  addImageBtnCLick: function(e) {
+    var that = this;
+    var imagesArray = that.data.selectedImageFilePaths;
+    if (imagesArray.length >= 2) {
+      wx.setTopBarText({
+        Text: '最多选择2张图片',
+      });
+    } else {
+      wx.chooseImage({
+        count: 2 - imagesArray.length,
+        success: function (res) {
+          if (res.tempFilePaths.length == 2) {
+            imagesArray = res.tempFilePaths;
+          } else {
+            imagesArray.push(res.tempFilePaths);
+          }
+          console.log(res);
+          that.setData({
+            selectedImageFilePaths: imagesArray,
+          });
+        },
+      });
+    }
   },
 
 })
